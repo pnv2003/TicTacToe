b0e84ced7b450b48034599284711c84c65741459
Phuong Ngo <phuongngovan2003@gmail.com>
2024-04-26 13:35:23 +0700

    Implement TicTacToe

---
+++ src/state.py
@@ -0,0 +1,81 @@
+from copy import deepcopy
+
+
+class State:
+    # marker
+    EMPTY = '.'
+    XMARK = 'X' # MAX player
+    OMARK = 'O' # MIN player
+    
+    # status
+    DRAW = '-'
+    
+    def __init__(self, h, w, k, to_move) -> None:
+        self.h = h # height
+        self.w = w # width
+        self.k = k # k in rows to win
+        
+        if to_move != State.XMARK and to_move != State.OMARK:
+            raise Exception("Not XO")
+        
+        self.to_move = to_move
+        self.status = State.DRAW
+        self.board = [
+            [
+                State.EMPTY
+                for j in range(self.w)
+            ]
+            for i in range(self.h)
+        ]
+        
+        self.empty_cells = {
+            (row, col)
+            for row in range(self.h)
+            for col in range(self.w)
+        }
+    
+    def out_of_board(self, row, col):
+        if 0 <= row < self.h and 0 <= col < self.w:
+            return False
+        return True
+    
+    def at(self, row, col):
+        
+        if self.out_of_board(row, col):
+            return None
+        
+        return self.board[row][col]
+    
+    def mark(self, row, col):
+            
+        if self.at(row, col) != State.EMPTY:
+            raise Exception("Marked")
+        
+        state = deepcopy(self)
+        
+        # state switch
+        state.board[row][col] = self.to_move
+        state.to_move = (
+            State.XMARK 
+            if self.to_move == State.OMARK 
+            else State.OMARK
+        )
+        state.empty_cells.remove((row, col))
+    
+        return state
+        
+    def possible_moves(self):
+        return list(self.empty_cells)
+    
+    def __repr__(self) -> str:
+        return (
+            f"Board to_move={self.to_move} status={self.status}\n" + 
+            '\n'.join([
+                ' '.join([
+                    self.at(row, col)
+                    for col in range(self.w)
+                ])
+                for row in range(self.h)
+            ]) + '\n'
+        )
+            
---
+++ src/ttt.py
@@ -0,0 +1,101 @@
+from src.game import Game
+from src.state import State
+
+class TicTacToe(Game):
+    MAXPL = State.XMARK # default MAX player
+    
+    def __init__(self, h=3, w=3, k=3) -> None:
+        self.h = h
+        self.w = w
+        self.k = k
+        self.initial = State(h, w, k, to_move=State.XMARK)
+    
+    def actions(self, state: State):
+        return state.possible_moves()
+    
+    def result(self, state: State, move):
+        
+        player = state.to_move
+        res = state.mark(*move)
+        
+        # compute utility
+        if self.k_in_row(res, player, move):
+            res.status = player
+        
+        return res
+    
+    def utility(self, state: State, player):
+        
+        # with open("./test.txt", "a") as f:
+        #     f.write("At state:")
+        #     f.write(str(state))
+        #     f.write("Got util:")
+        #     f.write(str(
+        #         (
+        #             1 if player == TicTacToe.MAXPL
+        #             else -1
+        #         ) if state.game_over
+        #         else 0
+        #     ))
+        #     f.write("\n")
+            
+        if state.status == State.DRAW:
+            return 0
+        
+        if state.status == player:
+            return 1
+        
+        return -1
+        
+    def terminal_test(self, state: State):
+        return state.status != State.DRAW or len(state.possible_moves()) == 0
+        
+    def to_move(self, state: State):
+        return state.to_move
+        
+    def k_in_row(self, state: State, player, move):
+        
+        dir = [
+            (0, 1), (1, 1), 
+            (1, 0), (1, -1)
+        ]
+                    
+        for dr, dc in dir:
+            
+            count = 0
+            
+            r, c = move    
+            while state.at(r, c) == player:
+                count += 1
+                r, c = r + dr, c + dc
+            
+            r, c = move    
+            while state.at(r, c) == player:
+                count += 1
+                r, c = r - dr, c - dc
+                
+            count -= 1
+            if count >= self.k:
+                return True
+            
+        return False
+    
+    def play_game(self, *players):
+        
+        turn = 1
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                print(f"Player {turn} move: {move}")
+                print(state)
+                if self.terminal_test(state):
+                    print(f"Player {turn} won!... or not")
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
+                
+                turn = 3 - turn
+    
+    
+    
