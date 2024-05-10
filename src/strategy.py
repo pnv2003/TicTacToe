d920eea5c32f130348da65ac3d35a3b64b284604
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-10 19:53:47 +0700

    Refactor

--- src/strategy.py
+++ src/strategy.py
@@ -1,93 +1,46 @@
 from functools import lru_cache
 import math
-import random
-from src.game import Game
-from src.state import State
 cache = lru_cache(10**6)
 
-def random_play(game: Game, state: State):
-    return random.choice(list(game.actions(state)))
+def alpha_beta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
 
-def minimax_search(game: Game, state: State):
     player = game.to_move(state)
-    
-    @cache
-    def max_value(state: State):
-        
-        if game.terminal_test(state):
-            return game.utility(state, player), None
-        
-        v = -math.inf
-        move = None
-        for a in game.actions(state):
-            
-            v2, a2 = min_value(game.result(state, a))
-            if v2 > v:
-                v, move = v2, a
-                
-        return v, move
-    
-    @cache
-    def min_value(state: State):
-        
-        if game.terminal_test(state):
-            return game.utility(state, player), None
-        
-        v = math.inf
-        move = None
-        for a in game.actions(state):
-            
-            v2, a2 = max_value(game.result(state, a))
-            if v2 < v:
-                v, move = v2, a
-                
-        return v, move
-    
-    value, move = max_value(state)
-    print(f"Value: {value}")
-    return move
 
-def alpha_beta_search(game: Game, state: State):
-    player = game.to_move(state)
-    
-    def max_value(state: State, alpha, beta):
-        
-        if game.terminal_test(state):
-            return game.utility(state, player), None
-        
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
         v = -math.inf
-        move = None
         for a in game.actions(state):
-            
-            v2, a2 = min_value(game.result(state, a), alpha, beta)
-            if v2 > v:
-                v, move = v2, a
-                alpha = max(alpha, v)
+            v = max(v, min_value(game.result(state, a), alpha, beta, depth + 1))
             if v >= beta:
-                return v, move
-            
-        return v, move
-    
-    def min_value(state: State, alpha, beta):
-        
-        if game.terminal_test(state):
-            return game.utility(state, player), None
-        
-        v = math.inf
-        move = None
-        for a in game.actions(state):
-            
-            v2, a2 = max_value(game.result(state, a), alpha, beta)
-            if v2 < v:
-                v, move = v2, a
-                beta = min(beta, v)
-            if v <= alpha:
-                return v, move
-            
-        return v, move
-    
-    value, move = max_value(state, -math.inf, math.inf)
-    return move
+                return v
+            alpha = max(alpha, v)
+        return v
 
-def monte_carlo_tree_search(game: Game, state: State):
-    pass
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = math.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a), alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or (lambda state, depth: depth > d or game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -math.inf
+    beta = math.inf
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta, 1)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
