a36a8e76afcc6c4b4de93db5fd0729bacb7d04bc
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-10 19:53:32 +0700

    Query and random players

---
+++ src/player.py
@@ -0,0 +1,23 @@
+import random
+
+
+def query_player(game, state):
+    """Make a move by querying standard input."""
+    print("current state:")
+    game.display(state)
+    print("available moves: {}".format(game.actions(state)))
+    print("")
+    move = None
+    if game.actions(state):
+        move_string = input('Your move? ')
+        try:
+            move = eval(move_string)
+        except NameError:
+            move = move_string
+    else:
+        print('no legal moves: passing turn to next player')
+    return move
+
+def random_player(game, state):
+    """A player that chooses a legal move at random."""
+    return random.choice(game.actions(state)) if game.actions(state) else None
