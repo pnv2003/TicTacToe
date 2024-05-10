818dd28ffea836f48b88f10856bb68efc071ae5b
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-10 19:53:02 +0700

    Refactor

--- src/ttt.py
+++ src/ttt.py
@@ -1,104 +1,72 @@
+from collections import namedtuple
 from src.game import Game
-from src.state import State
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
 
 class TicTacToe(Game):
-    MAXPL = State.XMARK # default MAX player
-    
-    def __init__(self, h=3, w=3, k=3) -> None:
+    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
+    A state has the player to move, a cached utility, a list of moves in
+    the form of a list of (x, y) positions, and a board, in the form of
+    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+
+    def __init__(self, h=3, v=3, k=3):
         self.h = h
-        self.w = w
+        self.v = v
         self.k = k
-        self.initial = State(h, w, k, to_move=State.XMARK)
-    
-    def actions(self, state: State):
-        return state.possible_moves()
-    
-    def result(self, state: State, move):
-        
-        player = state.to_move
-        res = state.mark(*move)
-        
-        # compute utility
-        if self.k_in_row(res, player, move):
-            res.status = player
-        
-        return res
-    
-    def utility(self, state: State, player):
-        
-        # with open("./test.txt", "a") as f:
-        #     f.write("At state:")
-        #     f.write(str(state))
-        #     f.write("Got util:")
-        #     f.write(str(
-        #         (
-        #             1 if player == TicTacToe.MAXPL
-        #             else -1
-        #         ) if state.game_over
-        #         else 0
-        #     ))
-        #     f.write("\n")
-            
-        if state.status == State.DRAW:
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
             return 0
-        
-        if state.status == player:
-            return 1
-        
-        return -1
-        
-    def terminal_test(self, state: State):
-        return state.status != State.DRAW or len(state.possible_moves()) == 0
-        
-    def to_move(self, state: State):
-        return state.to_move
-        
-    def k_in_row(self, state: State, player, move):
-        
-        dir = [
-            (0, 1), (1, 1), 
-            (1, 0), (1, -1)
-        ]
-                    
-        for dr, dc in dir:
-            
-            count = 0
-            
-            r, c = move    
-            while state.at(r, c) == player:
-                count += 1
-                r, c = r + dr, c + dc
-            
-            r, c = move    
-            while state.at(r, c) == player:
-                count += 1
-                r, c = r - dr, c - dc
-                
-            count -= 1
-            if count >= self.k:
-                return True
-            
-        return False
-    
-    def play_game(self, *players):
-        
-        turn = 1
-        state = self.initial
-        while True:
-            for player in players:
-                move = player(self, state)
-                state = self.result(state, move)
-                print(f"Player {turn} move: {move}")
-                print(state)
-                if self.terminal_test(state):
-                    if state.status == State.DRAW:
-                        print("Draw!")
-                    else:
-                        print(f"Player {turn} won!")
-                    self.display(state)
-                    return self.utility(state, self.to_move(self.initial))
-                
-                turn = 3 - turn
-    
-    
-    
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
