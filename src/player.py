f4d4e2473645ec22d6ed08fd9febe9ad5ab88cd8
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-10 19:58:13 +0700

    Here comes a pro player

--- src/player.py
+++ src/player.py
@@ -1,5 +1,5 @@
 import random
-
+from src.strategy import alpha_beta_cutoff_search
 
 def query_player(game, state):
     """Make a move by querying standard input."""
@@ -21,3 +21,10 @@ def query_player(game, state):
 def random_player(game, state):
     """A player that chooses a legal move at random."""
     return random.choice(game.actions(state)) if game.actions(state) else None
+
+def leveled_player(level = 1):
+    
+    def alpha_beta_player(game, state):
+        return alpha_beta_cutoff_search(game, state, d=level)
+    
+    return alpha_beta_player
