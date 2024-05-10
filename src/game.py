bdcd289deffb029ffc96d186e81b0e8f1ce9cec7
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-10 19:24:46 +0700

    Base model

--- src/game.py
+++ src/game.py
@@ -1,47 +1,36 @@
 class Game:
-    """A game is similar to a problem, but it has a utility for each
-    state and a terminal test instead of a path cost and a goal
-    test. To create a game, subclass this class and implement actions,
-    result, utility, and terminal_test. You may override display and
-    successors or you can inherit their default methods. You will also
-    need to set the .initial attribute to the initial state; this can
-    be done in the constructor."""
+    """A game is similar to a problem, but it has a terminal test instead of 
+    a goal test, and a utility for each terminal state. To create a game, 
+    subclass this class and implement `actions`, `result`, `is_terminal`, 
+    and `utility`. You will also need to set the .initial attribute to the 
+    initial state; this can be done in the constructor."""
 
     def actions(self, state):
-        """Return a list of the allowable moves at this point."""
+        """Return a collection of the allowable moves from this state."""
         raise NotImplementedError
 
     def result(self, state, move):
         """Return the state that results from making a move from a state."""
         raise NotImplementedError
 
+    def is_terminal(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+    
     def utility(self, state, player):
         """Return the value of this final state to player."""
         raise NotImplementedError
-
-    def terminal_test(self, state):
-        """Return True if this is a final state for the game."""
-        return not self.actions(state)
-
-    def to_move(self, state):
-        """Return the player whose move it is in this state."""
-        return state.to_move
-
-    def display(self, state):
-        """Print or otherwise display the state."""
-        print(state)
-
-    def __repr__(self):
-        return '<{}>'.format(self.__class__.__name__)
-
-    def play_game(self, *players):
-        """Play an n-person, move-alternating game."""
-        state = self.initial
-        while True:
-            for player in players:
-                move = player(self, state)
-                state = self.result(state, move)
-                if self.terminal_test(state):
-                    self.display(state)
-                    return self.utility(state, self.to_move(self.initial))
         
+
+def play_game(game, strategies: dict, verbose=False):
+    """Play a turn-taking game. `strategies` is a {player_name: function} dict,
+    where function(state, game) is used to get the player's move."""
+    state = game.initial
+    while not game.is_terminal(state):
+        player = state.to_move
+        move = strategies[player](game, state)
+        state = game.result(state, move)
+        if verbose: 
+            print('Player', player, 'move:', move)
+            print(state)
+    return state
